e8ee57286519a5c4032bc4594ba83a363bad8a08
Rohit <101079442+Singh8700@users.noreply.github.com>
2026-02-24 19:30:58 +0530

    fixed the deployment error

--- src/app/packages/page.jsx
+++ src/app/packages/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
-import {packages} from "../../models/packages"
+import {packages} from "../api/packages"
 
 
 const FeaturedPackages = () => {
